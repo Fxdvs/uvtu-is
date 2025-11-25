ae298d34a8c45654b04bb247be7c2937ba4f4b8a
Ladislav Spevák <ladislav.spevak@gmail.com>
2025-11-25 22:56:28 +0100

    Footer struc

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,15 @@
+import Logo from "@/components/logo";
+
 export default function Footer() {
     return (
-        <footer className="h-[20vh] w-full relative flex flex-col justify-center items-center border-t border-neutral-950" id="Footer">
-            <div className="h-full w-[]">
+        <footer className="h-[15vh] w-full relative flex flex-col justify-center items-center border-t border-neutral-950" id="Footer">
+            <div className="h-full w-[90%] flex justify-between items-center">
+                <div className="w-1/2 flex">
+                    <Logo className="text-neutral-100 size-7"/>
+                </div>
+                <div className="h-7 w-1/2">
 
+                </div>
             </div>
         </footer>
     )
