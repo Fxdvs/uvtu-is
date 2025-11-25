6cb6aaae287a64b0423a44ef0b368ed61eb4abf1
Ladislav Spevák <ladislav.spevak@gmail.com>
2025-11-25 22:55:58 +0100

    Added mini package

--- app/page.tsx
+++ app/page.tsx
@@ -115,11 +115,40 @@ export default function Page() {
           <FeaturesGrid />
         </motion.div>
       </section>
-      {/* */}
+      {/* Pricing */}
       <section
         id="Pricing"
         className="h-screen w-full flex justify-center items-center gap-4 relative"
       >
+        <div className="bg-neutral-50/10 border border-neutral-50/5 h-[40%] w-96 rounded-2xl flex flex-col relative p-5 gap-y-2 shadow shadow-neutral-50/10">
+          <GlowingEffect
+            spread={75}
+            glow={true}
+            disabled={false}
+            proximity={64}
+            inactiveZone={0.01}
+            borderWidth={2.5}
+          />
+          <div className="h-max w-full flex justify-between items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="size-10 text-neutral-100"
+              viewBox="0 0 16 16"
+            >
+              <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z" />
+            </svg>
+            <h1 className="text-3xl text-transparent bg-clip-text bg-gradient-to-br from-neutral-100 to-neutral-300 font-semibold uppercase tracking-wide">
+              2599€
+            </h1>
+          </div>
+          <h1 className="text-4xl text-transparent bg-clip-text bg-gradient-to-br from-neutral-100 to-neutral-300 font-semibold">
+            Mini
+          </h1>
+          <FeatureList type="mini"/>
+        </div>
         <div className="bg-neutral-50/10 border border-neutral-50/5 h-[50%] w-96 rounded-2xl flex flex-col relative p-5 gap-y-2 shadow shadow-neutral-50/10">
           <GlowingEffect
             spread={75}
@@ -142,10 +171,12 @@ export default function Page() {
             </svg>
             <h1 className="text-3xl text-transparent bg-clip-text bg-gradient-to-br from-neutral-100 to-neutral-300 font-semibold uppercase tracking-wide">
               4999€
-            </h1> 
+            </h1>
           </div>
-          <h1 className="text-4xl text-transparent bg-clip-text bg-gradient-to-br from-neutral-100 to-neutral-300 font-semibold">Basic</h1>
-          <FeatureList/>
+          <h1 className="text-4xl text-transparent bg-clip-text bg-gradient-to-br from-neutral-100 to-neutral-300 font-semibold">
+            Home
+          </h1>
+          <FeatureList type="home"/>
         </div>
         <div className="bg-neutral-50/10 border border-neutral-50/5 h-[60%] w-96 rounded-2xl flex flex-col relative p-5 gap-y-2">
           {" "}
@@ -175,8 +206,35 @@ export default function Page() {
               7499€
             </h1>
           </div>
-          <h1 className="text-4xl text-transparent bg-clip-text bg-gradient-to-br from-neutral-100 to-neutral-300 font-semibold">Extended</h1>
-          <FeatureList extended={true}/>
+          <h1 className="text-4xl text-transparent bg-clip-text bg-gradient-to-br from-neutral-100 to-neutral-300 font-semibold">
+            Pro
+          </h1>
+          <FeatureList type="pro"/>
+        </div>
+      </section>
+      <section className="h-screen w-full relative flex flex-col justify-center items-center">
+        <motion.div
+          className="size-full"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Prism
+            animationType="rotate"
+            timeScale={0.5}
+            height={3.6}
+            baseWidth={6.0}
+            scale={1.5}
+            hueShift={0}
+            colorFrequency={1}
+            noise={0.1}
+            glow={1}
+            offset={{ y: -50 }}
+          />
+        </motion.div>
+        <div className="size-full absolute flex flex-col items-center justify-center">
+          
         </div>
       </section>
       <Footer />
