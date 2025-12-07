59cb484b0413119143985c2aaf90f99af26e4368
Ladislav Spevák <ladislav.spevak@gmail.com>
2025-12-07 20:50:48 +0100

    Features page

---
+++ app/features/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+
+export default function Page() {
+  return (
+    <>
+      <Navbar />
+      <section
+        className="h-[150vh] w-full relative flex flex-col justify-center items-center"
+        id="Intro"
+      >
+        
+      </section>
+      <Footer/>
+    </>
+  );
+}
