6229624c647b9c12a795c4bd257ca075f318ac06
Ladislav Spevák <ladislav.spevak@gmail.com>
2025-12-11 13:47:20 +0100

    Changed base for features page

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -1,6 +1,6 @@
 "use client"
-import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer"; 
 
 export default function Page() {
   return (
